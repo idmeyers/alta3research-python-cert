ecfbd5677109074b0a1cc10555426a6f703c558e
idmeyers <32377914+idmeyers@users.noreply.github.com>
2022-06-15 15:29:26 -0400

    Create alta3research-pythoncert01.py
    Certification for python.

---
+++ alta3research-pythoncert01.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" create a program that asks for date range and returns the apod image(s) of that date range. Requires user to input api key. 
+created by Ian David Meyers """
+
+    
+import requests
+import json
+import webbrowser
+import time
+def main():    
+    #get the user input
+    
+    print("What is your api key? you will need to go to this url to request one: https://api.nasa.gov/")
+    api_key = input()
+    print("What is the date? i.E. 2017-01-01")
+    start_date = input()
+    print("What is the end date? i.E. 2017-01-02")
+    end_date = input()
+#create url from input
+    url = "https://api.nasa.gov/planetary/apod?api_key=mU3RPXkX6fMlmFNkMirrbauWTBfaPk02qs5wXgoP&start_date=" + start_date + "&end_date=" + end_date
+    print(url)      
+    #get the data
+    response = requests.get(url)
+    data = response.json()
+    
+    #get the image url
+    
+    for topic in data:
+        
+        #print("\n The next topic is ")
+        #print(topic)
+        image_url = topic.get("url")
+        #print(image_url)
+        #open the url in the browser
+        webbrowser.open(image_url)    
+        
+
+    
+    
+    #delay for large time frames
+    time.sleep(5)
+
+
+
+
+if __name__ == "__main__":
+    main()
